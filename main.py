ffd60f958d6e2049231bbeac02daa123734c2a20
Louis Jalouzot <ljalouzot1@gmail.com>
2024-10-30 18:03:46 +0100

    Fix vision context

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 from pathlib import Path
 
+import numpy as np
+
 from src.prompt_gemini import prompt_gemini
 
-issues = sorted(
-    [p.stem for p in Path("data/common_cause").glob("*.pdf")],
-    key=lambda x: int(x.split("_")[-1]),
-)
-issues = [
-    f"common_cause_{i}"
-    for i in [
-        1,
-        3,
-        4,
-        5,
-        11,
-        15,
-        29,
-        59,
-        100,
-        115,
-        130,
-        132,
-        159,
-        165,
-        184,
-        198,
-        200,
-        224,
-        252,
+np.random.seed(0)
+
+cc_sel = [
+    1,
+    3,
+    4,
+    5,
+    11,
+    15,
+    29,
+    59,
+    100,
+    115,
+    130,
+    132,
+    159,
+    165,
+    184,
+    198,
+    200,
+    224,
+    252,
+]
+issues = {
+    journal: [
+        p.stem
+        for p in Path(f"data/{journal}").glob("*.pdf")
+        if journal != "common_cause" or int(p.stem.split("_")[-1] not in cc_sel)
     ]
-] + ["suffragette_2", "votes_for_wmn_1"]
-prompt_gemini(issues)
+    for journal in ["common_cause", "suffragette", "votes_for_wmn"]
+}
+
+batch_cc = np.random.choice(issues["common_cause"], 40)
+batch_suffragettes = np.random.choice(issues["suffragette"], 10)
+batch_vfw = np.random.choice(issues["suffragette"], 7)
+batch_vfw = set(
+    list(batch_vfw) + [f"votes_for_wmn_{i}" for i in [100, 120, 140]]
+)
+
+batch = list(batch_cc) + list(batch_suffragettes) + list(batch_vfw)
+print(batch)
+prompt_gemini(batch)
